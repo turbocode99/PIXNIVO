40f90fd1fc5dbc081ac18eb41663f9e661a5e8ad
Abdullah <abdullahayaz.am@gmail.com>
2026-08-31 02:25:16 +0500

    Initial commit

---
+++ paddle.js
@@ -0,0 +1,11 @@
+/* esm.sh - ppu-paddle-ocr@6.4.3/web */
+import "/onnxruntime-web@^1.23.2?target=es2022";
+import "/ppu-ocv@^4.0.0/canvas-web?target=es2022";
+import "/ppu-paddle-ocr@6.4.3/es2022/constants.mjs";
+import "/ppu-paddle-ocr@6.4.3/es2022/core/base-detection.service.mjs";
+import "/ppu-paddle-ocr@6.4.3/es2022/core/base-paddle-ocr.service.mjs";
+import "/ppu-paddle-ocr@6.4.3/es2022/core/base-recognition.service.mjs";
+import "/ppu-paddle-ocr@6.4.3/es2022/core/session-factory.mjs";
+import "/ppu-paddle-ocr@6.4.3/es2022/model-catalogue.mjs";
+import "/ppu-paddle-ocr@6.4.3/es2022/utils.mjs";
+export * from "/ppu-paddle-ocr@6.4.3/es2022/web.mjs";
